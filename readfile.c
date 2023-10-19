d52ddfecd815b7c04842213a8188f6c40bb33d7b
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-19 07:03:25 +0100

    make function write from buffer

--- readfile.c
+++ readfile.c
@@ -30,9 +30,15 @@ size_t open_file(char *filename)
 int main(int ac, char *av[])
 {
 	/* declare variables */
-	ssize_t n_read, n_write;
+	ssize_t n_read, n_write, supply_count, buf_size;
 	int file_fd;
-	char buffer[BUFSIZE];
+	char *buffer, *new_mem;
+
+
+	supply_count = 0;
+	buf_size = BUFSIZE;
+
+	buffer = malloc(sizeof(char) * buf_size);
 
 
 	if (ac != 2)
@@ -43,28 +49,43 @@ int main(int ac, char *av[])
 
 	file_fd = open_file(av[1]);
 
-	while ((n_read = read(file_fd, buffer, BUFSIZE)) > 0)
+	while ((n_read = read(file_fd, buffer + supply_count,
+					buf_size - supply_count)) > 0)
 	{
-		n_write = write(STDOUT_FILENO, buffer, n_read);
+		supply_count += n_read;
 
-		if (n_write == -1 || n_write < n_read)
+		/* check if buffer is full and if so reallocate it */
+
+		if (supply_count >= buf_size)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to stdout");
-			close(file_fd);
-			exit(EXIT_FAILURE);
-		}
-	}
 
+			buf_size += BUFSIZE;
+			new_mem = realloc(buffer, buf_size);
+			if (!new_mem)
+				return (0);
+			/* Assign a new memory to buffer */
+			buffer = new_mem;
+		}
+		/* */
+
+	}
 	if (n_read == -1 || errno == EACCES)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		close(file_fd);
 		exit(EXIT_FAILURE);
 	}
-
 	close(file_fd);
+
+	n_write = write(STDOUT_FILENO, buffer, buf_size);
+	if (n_write == -1 || n_write < buf_size)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to stdout");
+		exit(EXIT_FAILURE);
+	}
 
 	return (0);
 
 }
 /* write the content of the file on stdout */
+
