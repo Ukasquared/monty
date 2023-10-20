acab27e14680b527baba4e8f81a070646090a3e7
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-20 15:55:41 +0100

    feat: add the function that prints from the list

---
+++ pall_funct.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * pall_func - prints the value on the stack
+ * @stack: linked list
+ * @line_number: number of each processed line
+ * Return: void
+ */
+
+void pall_func(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr;
+
+	/* check if null was passed into the function */
+	if (!stack && *stack == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%lu: usage: pall", line_number);
+		exit(EXIT_FAILURE);
+	}
+	/*loop backwards if the stack-prev is not NULL */
+	while ((*stack)->prev != NULL)
+		*stack = stack->prev;
+
+	ptr = *stack;
+	while (ptr != NULL)
+	{
+		printf("%d", ptr->data);
+		ptr = ptr->next;
+	}
+}
