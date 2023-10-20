b0e9b1b020c954850e79641a2f306fe55004b881
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-20 02:00:16 +0100

    added to the for loop

--- opcode_func.c
+++ opcode_func.c
@@ -1,9 +1,10 @@
 #include "monty.h"
 /**
- * accept_token - accepts token as an argument
+ * accept - accepts token as an argument
  * @token: the string token
  * @stack: the linked list
  * @line_number: linked list data
+ * Return: function pointer if succesful
  */
 
 void (*accept(char *token))(stack_t **stack, int line_number)
@@ -21,12 +22,18 @@ void (*accept(char *token))(stack_t **stack, int line_number)
 		{NULL, NULL},
 	};
 	/* loop through the array, compare and return corresponding function */
-	for (i = 0; i < 8; i++)
+	for (i = 0; stack_arr[i] != NULL; i++)
 	{
 		if (strcmp(stack_arr[i].opcode, token) == 0)
 		{
 			return (stack_func[i].f);
 		}
+	}
+	else
+	{
+		/* prints error to standard error stream */
+		dprintf(STDERR_FILENO, "L%lu: unknown instruction %s",
+				line_number, stack_arr[i])
 	}
 	/*returns NULL instead of pointer to function */
 	return (NULL);
