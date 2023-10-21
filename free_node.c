72e38fdfe057c04cac91cdb3e5d59ddad98c2177
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-21 12:09:14 +0100

    frees a node

---
+++ free_node.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * free_node - frees a node
+ * @stack_head:linked list
+ *
+ * Return: void
+ */
+
+void free_node(stack_t **stack_head)
+{
+	/* declare variables */
+	stack_t *currentNode;
+
+	/* initialize variable */
+	currentNode = NULL;
+
+	if (stack_head && *stack_head)
+	{
+		if ((*stack_head)->next != NULL)
+		{
+			currentNode = *stack_head;
+			*stack_head = (*stack_head)->next;
+			free(currentNode);
+		}
+	}
+}
