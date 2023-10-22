46ac851b845469d372340fc9b23ad710574f1ad2
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-22 01:18:39 +0100

    modify pall function and push function

--- pall_funct.c
+++ pall_funct.c
@@ -12,12 +12,12 @@ void pall_func(stack_t **stack_top, unsigned int line_number)
 	stack_t *ptr;
 
 	/* check if null was passed into the function */
-	if (!stack || *stack == NULL)
+	if (!stack_top || *stack_top == NULL)
 	{
 		dprintf(STDERR_FILENO, "L%lu: usage: pall\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	ptr = *stack;
+	ptr = *stack_top;
 	while (ptr != NULL)
 	{
 		line_number--;
