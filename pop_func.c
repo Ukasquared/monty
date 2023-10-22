ffe0687fae77f47445e25a22d49e8147d52d7e10
Ukasquared <ukachukwujudith95@gmail.com>
2023-10-22 01:03:06 +0100

    feat: add the pop function

--- pop_func.c
+++ pop_func.c
@@ -9,14 +9,11 @@
 
 void pop_funct(stack_t **stack_top, unsigned int line_number)
 {
-	stack_t *temp;
 	line_number--`;
 	if (!stack_top || *stack_top == NULL)
 	{
 		dprintf(STDERR_FILENO, " L%lu: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	temp = *stack_top;
-	*stack_top = (*stack_top)->next;
-	free_node(temp);
+	free_node(stack_top);
 }
